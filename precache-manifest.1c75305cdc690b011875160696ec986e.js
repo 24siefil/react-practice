f2deafe9c6b09d2f81fce78edad226c8d87cdd74
24siefil <24siefil@gmail.com>
2022-04-15 15:42:44 +0900

    Updates

---
+++ precache-manifest.1c75305cdc690b011875160696ec986e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f89397abda53ddf2b80181621635c31",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "45d8d90c2bed6f8f2a46",
+    "url": "/react-todo-list/static/js/2.54f18907.chunk.js"
+  },
+  {
+    "revision": "23a4cc73754d81f7e6b5",
+    "url": "/react-todo-list/static/js/main.fa6cccf6.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
