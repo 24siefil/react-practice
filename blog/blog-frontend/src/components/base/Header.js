ac2a3d2e4f4efd41adb74ba9a4b1ff537f59a446
24siefil <24siefil@gmail.com>
2022-05-27 19:54:10 +0900

    756

---
+++ blog/blog-frontend/src/components/base/Header.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export function Header() {
+
+  return (
+    <HeaderBlock>
+
+    </HeaderBlock>
+  );
+}
+
+export default Header;
+
+const HeaderBlock = styled.div`
+
+`;
